fe1c13876f2a6e10a4b25005ae1fdf56eb3c96d0
github username <github email address>
2017-04-13 13:27:28 -0400

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,16 @@
 class CreateStudents < ActiveRecord::Migration
+
+#   sql = <<-SQL
+#   CREATE TABLE students (
+#   id INTEGER PRIMARY KEY,
+#   name TEXT
+#   )
+#   SQL
+#
+# ActiveRecord::Base.connection.execute(sql)
+  def change
+    create_table :students do |t|
+    t.string :name
+    end
+  end
 end
